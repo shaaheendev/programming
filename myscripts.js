51c5221b22c0641ffa85aefdb249830a416f0c1e
LmnopEmc2 <110025657+LmnopEmc2@users.noreply.github.com>
2023-12-05 21:47:59 +0330

    v2.2

--- myscripts.js
+++ myscripts.js
@@ -4,6 +4,19 @@ function myFunction() {
 function myAddFunction() {
     document.getElementById("addCon").classList.toggle("bash");
   }
-  function myCover() {
+  /*function myCover() {
     document.getElementById("coverPage").classList.toggle("bash");
+  }*/
+  //experiment
+  window.onclick = function(event) {
+    if (!event.target.matches('.btn-m', 'cover-page')) {
+      var dropdowns = document.getElementsByClassName("menu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('bash')) {
+          openDropdown.classList.remove('bash');
+        }
+      }
+    }
   }
