59dd7adaf12ce327497dcbcce2799dd76fd83ad2
LmnopEmc2 <110025657+LmnopEmc2@users.noreply.github.com>
2023-12-05 17:41:24 +0330

    v2.1

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("menOp").classList.toggle("bash");
+  }
+function myAddFunction() {
+    document.getElementById("addCon").classList.toggle("bash");
+  }
+  function myCover() {
+    document.getElementById("coverPage").classList.toggle("bash");
+  }
