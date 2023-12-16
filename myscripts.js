44361c56f3e18d17420b4557c497acdf66daa970
LmnopEmc2 <110025657+LmnopEmc2@users.noreply.github.com>
2023-12-16 20:32:08 +0330

    Update myscripts.js v.m

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,36 @@
+let i = 0;
 function myFunction() {
-    document.getElementById("menOp").classList.toggle("bash");
-  }
-function myAddFunction() {
-    document.getElementById("addCon").classList.toggle("bash");
-  }
-  /*function myCover() {
-    document.getElementById("coverPage").classList.toggle("bash");
-  }*/
-  //experiment
-  window.onclick = function(event) {
-    if (!event.target.matches('.btn-m')) {
-      var dropdowns = document.getElementsByClassName("menu-content",);
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('bash')) {
-          openDropdown.classList.remove('bash');
-        }
-      }
-    }
-  }
-  function rmvFunc() {
-    document.getElementById("addCon").classList.remove("bash");
-  }
+				i++;
+     document.getElementById("menOp").classList.toggle("bash");
+   } 
+ function myAddFunction() { 
+ 				i++;
+     document.getElementById("addCon").classList.toggle("bash"); 
+   } 
+   /*function myCover() { 
+     document.getElementById("coverPage").classList.toggle("bash"); 
+   }*/ 
+   //experiment
+window.onclick = () =>  { 
+     if (!event.target.matches('.btn, .menu-button, .line, .menu, .menu-content, a, .options, .add-button, .op-button, .sub-add, .add-page, label, input') && i !== 0) {
+           i = 0;
+           document.getElementById("menOp").classList.remove('bash');
+     				  document.getElementById("addCon").classList.remove("bash"); 
+       } 
+     }
+/*   function rmvFunc() { 
+     document.getElementById("addCon").classList.remove("bash"); 
+   }
+window.onclick
+= function(event1) { 
+     if (!event1.target.matches('.btn-add')) { 
+       var dropdownsAdd = document.getElementsByClassName("add-page",); 
+       var j; 
+       for (j = 0; j < dropdownsAdd.length; j++) { 
+         var openDropdownAdd = dropdownsAdd[j]; 
+         if (openDropdownAdd.classList.contains('bash')) { 
+           openDropdownAdd.classList.remove('bash'); 
+         } 
+       } 
+     } 
+   } */
